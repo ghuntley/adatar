e8f9233c8988a68fc38a67cfbaa808cff00e62e3
Geoffrey Huntley <ghuntley@ghuntley.com>
2015-02-18 15:05:14 +1100

    initial commit :rocket:

---
+++ src/Adatar/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Adatar")>]
+[<assembly: AssemblyProductAttribute("Adatar")>]
+[<assembly: AssemblyDescriptionAttribute("Adatar is a Active Directory microservice for employee photos, with optional fallback to Gravatar.com")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
